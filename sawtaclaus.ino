dfe354901640f7658e1b22c32a3bad12b5c6cebc
Raimundo Lorca Correa <89880069+rlorcac@users.noreply.github.com>
2023-05-05 02:40:36 -0400

    funcional para el sabado
    por reemplazar numeros de pines

--- sawtaclaus.ino
+++ sawtaclaus.ino
@@ -4,22 +4,29 @@
 #include <ESP32PWM.h>
 #include <Ps3Controller.h>
 
-Serial.begin(9600)
-
-
-  class Rueda {
+class Rueda {
 public:
   int fwdPin, backwdPin;
-  Rueda(int _fwdPin, int _backwdPin) {
+  String name;
+  Rueda(int _fwdPin, int _backwdPin, String _name) {
     pinMode(fwdPin, OUTPUT);
     pinMode(backwdPin, OUTPUT);
     fwdPin = _fwdPin;
     backwdPin = _backwdPin;
+    name = _name;
   }
-  void desp(int spd) {  // spd: -255 ~ 255
-    spd = map(spd, -128, 127, -255, 255);
+  void desplazar(int spd) {  // spd: -255 ~ 255
+    Serial.print("Moviendo rueda: ");
+    Serial.print(name);
+    Serial.print(" a velocidad: ");
+
+    spd = map(spd, -128, 128, -255, 255);
     spd = min(255, spd);
     spd = max(-255, spd);
+
+    Serial.print(spd, DEC);
+    Serial.println();
+
     if (spd >= 0) {
       analogWrite(backwdPin, 0);
       analogWrite(fwdPin, spd);
@@ -36,44 +43,98 @@ public:
   int pin;
   Servo servo;
   int minMus, maxMus, stopMus;  // (microSeconds)
-  bool lock;
   Arma(int _pin, int _minMus, int _maxMus, int _stopMus) {
     pinMode(pin, OUTPUT);
     pin = _pin;
     minMus = _minMus;
     maxMus = _maxMus;
     stopMus = _stopMus;
+    lock = true;
     servo.attach(pin);
     servo.writeMicroseconds(stopMus);
   }
-  void activar(int spd) {  //spd: 0 ~ 100
+  void activar(int spd) {  //spd: 0 ~ 255
     if (!lock) {
-      spd = map(spd, 0, 100, minMus, maxMus);
+      spd = map(spd, 0, 255, minMus, maxMus);
       spd = min(maxMus, spd);
       spd = max(minMus, spd);
+
+      Serial.print("Arma activada a velocidad: ");
+      Serial.print(spd, DEC);
+      Serial.println();
+
       servo.writeMicroseconds(spd);
+    } else {
+      Serial.println("El arma no se activó porque estaba lockeada");
     }
   }
   void lockOn() {
+    Serial.println("Lock del arma activado");
+    servo.writeMicroseconds(stopMus);
     lock = true;
   }
   void lockOff() {
+    Serial.println("Lock del arma desactivado");
     lock = false;
   }
+private:
+  bool lock;
 };
 
-const int MOVS[4] = { 11, 10, 9, 3 };  //RF, RB, LF, LB ->  IN4, IN3, IN2, IN1
-const int LED = 4;
+// CONSTANTES
+const int MOVS[4] = { 5, 5, 5, 5 };  //RF, RB, LF, LB ->  IN4, IN3, IN2, IN1
 const int ARMA = 5;
 const int minArma = 1300, maxArma = 2000, stopArma = 1300;  // calibración manual
-const Rueda ruedaDer = Rueda(MOVS[0], MOVS[1]), ruedaIzq = Rueda(MOVS[2], MOVS[3]);
-const Arma arma = Arma(ARMA, minArma, maxArma, stopArma);
+Rueda ruedaDer = Rueda(MOVS[0], MOVS[1], "Derecha"), ruedaIzq = Rueda(MOVS[2], MOVS[3], "Izquierda");
+Arma arma = Arma(ARMA, minArma, maxArma, stopArma);
 
 void onConnect() {
   Serial.println("Connected.");
 }
 
 void controles() {
+  sticks_tanque();
+  controles_arma();
+}
+
+void sticks_tanque() {
+  int der = 0, izq = 0, deadzone = 15;
+  // ------------------------------ Sticks (Analógicos) ------------------------------
+  izq = -Ps3.data.analog.stick.ly;
+  izq = (abs(izq) > deadzone) ? izq : 0;
+  ruedaIzq.desplazar(izq);
+
+  der = -Ps3.data.analog.stick.ry;
+  der = (abs(der) > deadzone) ? der : 0;
+  ruedaDer.desplazar(der);
+}
+
+void sticks_auto() {
+  int v = 0, omega = 0, deadzone = 15, der = 0, izq = 0;
+  // ------------------------------ Sticks (Analógicos) ------------------------------
+  omega = Ps3.data.analog.stick.lx;
+  omega = (abs(omega) > deadzone) ? omega : 0;
+  v = -Ps3.data.analog.stick.ry;
+  v = (abs(v) > deadzone) ? v : 0;
+  // Cinemática inversa (por definir)
+  float om_rate = 0.5, radius = 1;
+  izq = (v - 0.5 * omega * om_rate) / radius;
+  der = (v + 0.5 * omega * om_rate) / radius;
+
+  ruedaIzq.desplazar(int(izq));
+  ruedaDer.desplazar(int(der));
+}
+
+void controles_arma() {
+  // ------------------------------ Triggers (Analógicos) ------------------------------
+  arma.activar(Ps3.data.analog.button.r2);
+  // ------------------------------ Pad Derecho (Digital) ------------------------------
+  if (Ps3.event.button_down.cross) {
+    arma.lockOn();
+  }
+  if (Ps3.event.button_down.circle) {
+    arma.lockOff();
+  }
 }
 
 void setup() {
@@ -87,4 +148,7 @@ void setup() {
 }
 
 void loop() {
+  if (!Ps3.isConnected())
+    return;
+  delay(2000);
 }
