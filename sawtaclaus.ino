94a42e6b770855315db232064364a3bf8f54513a
Raimundo Lorca Correa <89880069+rlorcac@users.noreply.github.com>
2023-05-07 22:15:02 -0400

    script final

--- sawtaclaus.ino
+++ sawtaclaus.ino
@@ -1,104 +1,110 @@
-#include <ESP32Servo.h>
-#include <analogWrite.h>
-#include <ESP32Tone.h>
-#include <ESP32PWM.h>
+#include <Servo.h>
 #include <Ps3Controller.h>
+#include <analogWrite.h>
 
 class Rueda {
-public:
-  int fwdPin, backwdPin;
-  String name;
-  Rueda(int _fwdPin, int _backwdPin, String _name) {
-    pinMode(fwdPin, OUTPUT);
-    pinMode(backwdPin, OUTPUT);
-    fwdPin = _fwdPin;
-    backwdPin = _backwdPin;
-    name = _name;
-  }
-  void desplazar(int spd) {  // spd: -255 ~ 255
-
-    spd = map(spd, -128, 128, -255, 255);
-    spd = min(255, spd);
-    spd = max(-255, spd);
-
-    if (spd) {
-      Serial.print("Moviendo rueda: ");
-      Serial.print(name);
-      Serial.print(" a velocidad: ");
-      Serial.print(spd, DEC);
-      Serial.println();
+  public:
+    int fwdPin, backwdPin;
+    int fwdCH, backwdCH;
+    String name;
+    Rueda(int _fwdPin, int _backwdPin, int _fwdCH, int _backwdCH, String _name) {
+      pinMode(fwdPin, OUTPUT);
+      pinMode(backwdPin, OUTPUT);
+      fwdPin = _fwdPin;
+      backwdPin = _backwdPin;
+      name = _name;
     }
+    void desplazar(int spd) {  // spd: -255 ~ 255
 
-    if (spd >= 0) {
-      analogWrite(backwdPin, 0);
-      analogWrite(fwdPin, spd);
-    }
-    if (spd <= 0) {
-      analogWrite(fwdPin, 0);
-      analogWrite(backwdPin, -spd);
-    }
-  }
-};
+      spd = map(spd, -128, 128, -255, 255);
+      spd = min(255, spd);
+      spd = max(-255, spd);
 
-class Arma {
-public:
-  int pin;
-  Servo servo;
-  int minMus, maxMus, stopMus;  // (microSeconds)
-  Arma(int _pin, int _minMus, int _maxMus, int _stopMus) {
-    pinMode(pin, OUTPUT);
-    pin = _pin;
-    minMus = _minMus;
-    maxMus = _maxMus;
-    stopMus = _stopMus;
-    lock = true;
-    servo.attach(pin);
-    servo.writeMicroseconds(stopMus);
-  }
-  void activar(int spd) {  //spd: 0 ~ 255
-    if (!lock) {
-      spd = map(spd, 0, 255, minMus, maxMus);
-      spd = min(maxMus, spd);
-      spd = max(minMus, spd);
-
-      if (spd != stopMus) {
-        Serial.print("Arma activada a velocidad: ");
+      if (spd) {
+        Serial.print("Moviendo rueda: ");
+        Serial.print(name);
+        Serial.print(" a velocidad: ");
         Serial.print(spd, DEC);
         Serial.println();
       }
-
-      servo.writeMicroseconds(spd);
-    } else {
-      if (spd)
-        Serial.println("El arma no se activó porque estaba lockeada");
+      if (spd >= 0) {
+        analogWrite(backwdPin, 0);
+        analogWrite(fwdPin, spd);
+      }
+      if (spd <= 0) {
+        analogWrite(fwdPin, 0);
+        analogWrite(backwdPin, -spd);
+      }
     }
-  }
-  void lockOn() {
-    Serial.println("Lock del arma activado");
-    servo.writeMicroseconds(stopMus);
-    lock = true;
-  }
-  void lockOff() {
-    Serial.println("Lock del arma desactivado");
-    lock = false;
-  }
-private:
-  bool lock;
+};
+
+class Arma {
+  public:
+    int pin, channel;
+    Servo servo;
+    int minMus, maxMus, stopMus;  // (microSeconds)
+    Arma(int _pin, int _channel, int _minMus, int _maxMus, int _stopMus, int _freq) {
+      pinMode(pin, OUTPUT);
+      pin = _pin;
+      minMus = _minMus;
+      maxMus = _maxMus;
+      stopMus = _stopMus;
+      channel = _channel;
+      lock = true;
+
+      ledcSetup(channel, _freq, 8);
+
+      servo.attach(pin, channel);
+      servo.writeMicroseconds(stopMus);
+    }
+    void activar(int spd) {  //spd: 0 ~ 255
+      if (!lock) {
+        spd = map(spd, 0, 255, minMus, maxMus);
+        spd = min(maxMus, spd);
+        spd = max(minMus, spd);
+
+        if (spd != stopMus) {
+          Serial.print("Arma activada a velocidad: ");
+          Serial.print(spd, DEC);
+          Serial.println();
+        }
+
+        servo.writeMicroseconds(spd);
+      } else {
+        if (spd)
+        
+          Serial.println("El arma no se activó porque estaba lockeada");
+      }
+    }
+    void lockOn() {
+      Serial.println("Lock del arma activado");
+      lock = true;
+      servo.writeMicroseconds(stopMus);
+      Serial.println("Arma detenida");
+    }
+    void lockOff() {
+      Serial.println("Lock del arma desactivado");
+      lock = false;
+    }
+  private:
+    bool lock;
 };
 
 // CONSTANTES
-const int MOVS[4] = { 5, 5, 5, 5 };  //RF, RB, LF, LB ->  IN4, IN3, IN2, IN1
-const int ARMA = 5;
+const int PWM_FREQ = 50;
+const int MOVS[4] = { 16, 17, 19, 18 };  //RF, RB, LF, LB ->  IN4, IN3, IN2, IN1
+const int ARMA = 13;
 const int minArma = 1300, maxArma = 2000, stopArma = 1300;  // calibración manual
-Rueda ruedaDer = Rueda(MOVS[0], MOVS[1], "Derecha"), ruedaIzq = Rueda(MOVS[2], MOVS[3], "Izquierda");
-Arma arma = Arma(ARMA, minArma, maxArma, stopArma);
+Rueda ruedaDer = Rueda(MOVS[0], MOVS[1], 0, 1, "Derecha"), ruedaIzq = Rueda(MOVS[2], MOVS[3], 2, 3, "Izquierda");
+Arma arma = Arma(ARMA, 4, minArma, maxArma, stopArma, PWM_FREQ);
+// CONSTANTES
 
 void onConnect() {
   Serial.println("Connected.");
 }
 
 void controles() {
-  sticks_tanque();
+  sticks_auto();
   controles_arma();
 }
 
@@ -107,10 +113,10 @@ void sticks_tanque() {
   // ------------------------------ Sticks (Analógicos) ------------------------------
   izq = -Ps3.data.analog.stick.ly;
   izq = (abs(izq) > deadzone) ? izq : 0;
-  ruedaIzq.desplazar(izq);
-
   der = -Ps3.data.analog.stick.ry;
   der = (abs(der) > deadzone) ? der : 0;
+  
+  ruedaIzq.desplazar(izq);
   ruedaDer.desplazar(der);
 }
 
@@ -121,8 +127,9 @@ void sticks_auto() {
   omega = (abs(omega) > deadzone) ? omega : 0;
   v = -Ps3.data.analog.stick.ry;
   v = (abs(v) > deadzone) ? v : 0;
+  
   // Cinemática inversa (por definir)
-  float om_rate = 0.5, radius = 1;
+  float om_rate = 1, radius = 1;
   izq = (v - 0.5 * omega * om_rate) / radius;
   der = (v + 0.5 * omega * om_rate) / radius;
 
@@ -142,18 +149,22 @@ void controles_arma() {
   }
 }
 
+void btconnect() {
+  Ps3.begin("00:22:02:01:11:0e");
+}
+
 void setup() {
   Serial.begin(115200);
 
   Ps3.attach(controles);
   Ps3.attachOnConnect(onConnect);
-  Ps3.begin("00:22:02:01:11:0e");
+  btconnect();
 
   Serial.println("Ready.");
 }
 
 void loop() {
   if (!Ps3.isConnected())
-    return;
+    btconnect();
   delay(2000);
 }
